728dac26851db0de8dfd0d1d8232662363133003
Paragoumba <paragoumba@gmail.com>
2020-05-27 12:49:36 +0200

    Lots of improvements

--- src/main/resources/js/script.js
+++ src/main/resources/js/script.js
@@ -1,15 +1,40 @@
+const loader = new THREE.TextureLoader();
+const movement = {
+    forward: false,
+    left: false,
+    backward: false,
+    right: false,
+}
+
+const materials = {
+    white: new THREE.MeshPhongMaterial({color: 0xFFFFFF}),
+    black: new THREE.MeshPhongMaterial({color: 0x000000}),
+    red: new THREE.MeshPhongMaterial({color: 0xFF0000}),
+    green: new THREE.MeshPhongMaterial({color: 0x00FF00}),
+    blue: new THREE.MeshPhongMaterial({color: 0x0000FF}),
+    yellow: new THREE.MeshPhongMaterial({color: 0xFFFF00}),
+    cyan: new THREE.MeshPhongMaterial({color: 0x00FFFF}),
+    magenta: new THREE.MeshPhongMaterial({color: 0xFF00FF}),
+    brick: new THREE.MeshPhongMaterial({map: loader.load('imgs/brick.png')}),
+    stone: new THREE.MeshPhongMaterial({map: loader.load('imgs/stone.png')}),
+    icon: new THREE.MeshPhongMaterial({map: loader.load('imgs/3dlab-icon.png')})
+}
 const textElt = document.getElementById("text");
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer({antialias: true});
+const renderer = new THREE.WebGLRenderer({antialias: false});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-/*var controls = new THREE.PointerLockControls(camera, document.body);
-controls.movementSpeed = 1000;
-controls.lookSpeed = 0.1;*/
+const controls = new THREE.PointerLockControls(camera, renderer.domElement);
+
+renderer.domElement.addEventListener('click', function(){
+
+    controls.lock();
+
+}, false);
 
 window.addEventListener('resize', function(event){
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -17,47 +42,79 @@ window.addEventListener('resize', function(event){
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshPhongMaterial({color: 0xFFFFFF});
-const skyBox = new THREE.Mesh(geometry, material);
+scene.background = new THREE.CubeTextureLoader().load(['imgs/cloudtop_ft.png', 'imgs/cloudtop_bk.png', 'imgs/cloudtop_up.png', 'imgs/cloudtop_dn.png', 'imgs/cloudtop_rt.png', 'imgs/cloudtop_lf.png']);
+scene.add(new THREE.AmbientLight(0xAAAAAA));
 
-skyBox.flipSided = false;
-skyBox.position.x = 4.5;
-skyBox.position.z = 4.5;
-skyBox.scale.x = 20;
-skyBox.scale.y = 20;
-skyBox.scale.z = 20;
-
-//scene.add(controls.getObject());
-scene.add(skyBox);
-
-scene.add(new THREE.AmbientLight(0x222222));
-
-var light = new THREE.DirectionalLight();
-light.position.set(4.5, 5, 4.5);
-//light.target.set(5, 10, 5);
+var light = new THREE.PointLight();
 light.castShadow = true;
 light.shadow.camera.zoom = 4; // tighter shadow map
 scene.add(light);
-//controls.lock();
-
-camera.position.x = 4.5;
-camera.position.y = 10;
-camera.position.z = 4.5;
-
-camera.rotation.x = -Math.PI / 2;
 
 let lastFPS = 0;
 let fps = 0;
 let last = new Date().getTime();
 
+function keyEvent(event, pressed){
+
+    switch (event.keyCode){
+
+        case 90: // z
+            movement.forward = pressed;
+            break;
+
+        case 81: // q
+            movement.left = pressed;
+            break;
+
+        case 83: // s
+            movement.backward = pressed;
+            break;
+
+        case 68: // d
+            movement.right = pressed;
+            break;
+
+    }
+}
+
+document.addEventListener( 'keydown', (ev) => {keyEvent(ev, true)}, false);
+document.addEventListener( 'keyup', (ev) => {keyEvent(ev, false)}, false);
+
 function animate(){
 
     requestAnimationFrame(animate);
-    //camera.rotation.x = Math.sin(new Date().getTime() / 1000) / 2;
-    //camera.rotation.y = Math.sin(new Date().getTime() / 1000) / 2;
-    //camera.rotation.z = Math.sin(new Date().getTime() / 1000) / 2;
+
     let now = new Date().getTime();
+
+    if (controls.isLocked === true){
+
+        if (movement.forward){
+
+            controls.moveForward(1);
+
+        }
+
+        if (movement.left){
+
+            controls.moveRight(-1);
+
+        }
+
+        if (movement.backward){
+
+            controls.moveForward(-1);
+
+        }
+
+        if (movement.right){
+
+            controls.moveRight(1);
+
+        }
+
+        //light.position.set(camera.position.x, camera.position.y, camera.position.z);
+
+    }
 
     if (now - last > 1000){
 
@@ -67,7 +124,8 @@ function animate(){
 
     }
 
-    textElt.textContent = "x:" + 180 / Math.PI * camera.rotation.x + " y:" + 180 / Math.PI * camera.rotation.y + " z:" + 180 / Math.PI * camera.rotation.z + " FPS:" + lastFPS;
+    //textElt.textContent = "x:" + (180 / Math.PI * camera.rotation.x).toFixed(2) + " y:" + (180 / Math.PI * camera.rotation.y).toFixed(2) + " z:" + (180 / Math.PI * camera.rotation.z).toFixed(2) + " FPS:" + lastFPS;
+    textElt.textContent = "x:" + (camera.position.x).toFixed(2) + " y:" + (camera.position.y).toFixed(2) + " z:" + (camera.position.z).toFixed(2) + " FPS:" + lastFPS;
     renderer.render(scene, camera);
     ++fps;
 
@@ -112,7 +170,14 @@ function importLab(data){
         for (const block of lab.walls){
 
             const geometry = new THREE.BoxGeometry();
-            const material = new THREE.MeshPhongMaterial({color: 0xFFFFFF});
+            let material = materials[block.material];
+
+            if (material === undefined){
+
+                material = new THREE.MeshPhongMaterial();
+
+            }
+
             const cube = new THREE.Mesh(geometry, material);
 
             cube.position.x = block.x;
@@ -130,10 +195,11 @@ function importLab(data){
                 if (object === undefined){
 
                     const geometry = new THREE.PlaneGeometry();
-                    const material = new THREE.MeshPhongMaterial({color: 0xFF0000});
+                    const material = materials.white;
                     object = new THREE.Mesh(geometry, material);
 
                     object.position.x = x;
+                    object.position.y = -0.5;
                     object.position.z = y;
                     object.rotation.x = -Math.PI / 2;
                     newLevel.lab.rows[x][y] = object;
@@ -148,4 +214,14 @@ function importLab(data){
         ++i;
 
     }
+
+    const halfWidth = levels[0].lab.width / 2;
+    const halfHeight = levels[0].lab.height / 2;
+
+    camera.position.set(halfWidth, 20, halfHeight);
+
+    camera.rotation.x = -Math.PI / 2;
+
+    light.position.set(halfWidth, 15, halfHeight);
+
 }
