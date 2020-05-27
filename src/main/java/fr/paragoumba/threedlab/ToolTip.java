582866987767622c76476b3fca1bcbb8e636668e
Paragoumba <paragoumba@gmail.com>
2020-05-27 13:40:57 +0200

    Added licenses and style

---
+++ src/main/java/fr/paragoumba/threedlab/ToolTip.java
@@ -0,0 +1,53 @@
+package fr.paragoumba.threedlab;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static fr.paragoumba.threedlab.BlueprintPanel.PIXEL_SIZE;
+
+public class ToolTip extends JToolTip {
+
+    public ToolTip(){
+
+        setBorder(null);
+
+        Dimension size = new Dimension(PIXEL_SIZE * 10, PIXEL_SIZE * 6);
+
+        setMinimumSize(size);
+        setMaximumSize(size);
+        setPreferredSize(size);
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g){
+
+        int width = getWidth();
+        int height = getHeight();
+        
+        g.setColor(BlueprintPanel.bgColor);
+        g.fillRect(0, PIXEL_SIZE, width, height - 2 * PIXEL_SIZE);
+        g.fillRect(PIXEL_SIZE, 0, width - 2 * PIXEL_SIZE, PIXEL_SIZE);
+        g.fillRect(PIXEL_SIZE, height - PIXEL_SIZE, width - 2 * PIXEL_SIZE, PIXEL_SIZE);
+
+        g.setColor(BlueprintPanel.brightShadow);
+        g.fillRect(PIXEL_SIZE, 0, width - 2 * PIXEL_SIZE, PIXEL_SIZE);
+        g.fillRect(PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
+        g.fillRect(0, PIXEL_SIZE, PIXEL_SIZE, height - 2 * PIXEL_SIZE);
+
+        g.setColor(BlueprintPanel.darkShadow);
+        g.fillRect(PIXEL_SIZE, height - PIXEL_SIZE, width - 2 * PIXEL_SIZE, PIXEL_SIZE);
+        g.fillRect(width - PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE, height - 2 * PIXEL_SIZE);
+        g.fillRect(width - 2 * PIXEL_SIZE, height - 2 * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
+
+        g.setFont(new Font(BlueprintPanel.font.getName(), Font.PLAIN, 12));
+
+        String text = getTipText();
+        int stringWidth = g.getFontMetrics().stringWidth(text);
+        int stringHeight = g.getFontMetrics().getHeight();
+
+        g.setColor(Color.WHITE);
+        g.drawString(text, (width - stringWidth) / 2, (height + stringHeight) / 2);
+
+    }
+}
