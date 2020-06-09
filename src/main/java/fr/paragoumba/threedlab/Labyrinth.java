7c93e75108a975a0aee47fedf8a3454d3f8a71c3
Paragoumba <paragoumba@gmail.com>
2020-06-09 19:54:44 +0200

    Added start and end points

--- src/main/java/fr/paragoumba/threedlab/Labyrinth.java
+++ src/main/java/fr/paragoumba/threedlab/Labyrinth.java
@@ -2,12 +2,16 @@ package fr.paragoumba.threedlab;
 
 import fr.paragoumba.threedlab.materials.Material;
 
+import java.awt.*;
+
 public class Labyrinth {
 
     public Labyrinth(int width, int height){
 
         this.width = width;
         this.height = height;
+        this.start = new Point(0, 0);
+        this.end = new Point(width - 1, height - 1);
         this.grid = new Material[width][height];
 
     }
@@ -15,6 +19,8 @@ public class Labyrinth {
     private final Material[][] grid;
     private final int width;
     private final int height;
+    private Point start;
+    private Point end;
 
     public void toggleSquare(int x, int y, Material material){
 
@@ -40,6 +46,30 @@ public class Labyrinth {
     public int getHeight(){
 
         return height;
+
+    }
+
+    public Point getStart(){
+
+        return start;
+
+    }
+
+    public Point getEnd(){
+
+        return end;
+
+    }
+
+    public void setStart(Point start){
+
+        this.start = start;
+
+    }
+
+    public void setEnd(Point end){
+
+        this.end = end;
 
     }
 }
