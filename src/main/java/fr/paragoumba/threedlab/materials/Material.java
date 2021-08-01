2a4612a1cebc08ef0f20b91cd299d41b553bb6c0
Paragoumba <paragoumba@gmail.com>
2021-08-01 17:15:24 +0200

    Materials can now be deserialized

--- src/main/java/fr/paragoumba/threedlab/materials/Material.java
+++ src/main/java/fr/paragoumba/threedlab/materials/Material.java
@@ -13,6 +13,19 @@ public abstract class Material {
 
     }
 
+    public static Material getMaterial(String material){
+
+        if (material.startsWith("#")){
+
+            return new ColorMaterial(new Color(Integer.parseInt(material.substring(1), 16)));
+
+        } else {
+
+            return new TextureMaterial("/imgs/" + material + ".png");
+
+        }
+    }
+
     public abstract void drawMaterial(Graphics g);
 
 }
